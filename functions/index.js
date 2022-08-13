8026e321a0ca2e77c049a7ba78caf0380eb920b7
wnrakvp <wnr.akvp@gmail.com>
2022-08-13 22:45:29 +0700

    JSONServer;

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,8 @@
 const functions = require('firebase-functions');
-const key = functions.config().stripe.key;
+// const key = functions.config().stripe.key;
 const {detailedDiff} = require('deep-object-diff');
 const axios = require('axios');
+let initApp = 0;
 // const connectDB = require('./config/connectDB');
 // const serviceAccount = require('./config/service-account/database.json');
 
@@ -13,97 +14,109 @@ const axios = require('axios');
 
 exports.getProduct = functions.https.onRequest((req, res) => {
   const admin = require('firebase-admin');
-  admin.initializeApp();
-  try {
-    // const ref = await connectDB(admin, 'testDB');
-    const db = admin.database();
-    const ref = db.ref();
-    ref.on(
-        'value',
-        (snapshot) => {
-          functions.logger.info(snapshot.val(), {structuredData: true});
-          return res.status(200).json(snapshot.val());
-        },
-        (errorObject) => {
-          console.log('The read failed: ' + errorObject.name);
-          throw new Error('Cannot Read Data.');
-        }
-    );
-  } catch (e) {
-    console.log(e.message);
-    return res.status(500).json({msg: 'Internal Server Error'});
+  if (initApp === 0) {
+    admin.initializeApp();
+    initApp = 1;
   }
+  // Mockup Scraper Website
+  axios.get('http://localhost:3000/products')
+      .then((response) => {
+        const db = admin.firestore();
+        db.collection('BRAND').doc('products').set(response.data)
+            .then((response) => {
+              return res.status(200).json({msg: 'Added Successfully',
+                data: response});
+            })
+            .catch((err)=> {
+              console.error(err);
+              return res.status(500).json({msg: 'Internal Server Error'});
+            });
+      })
+      .catch((err)=> {
+        console.log(err);
+        return res.status(500).json({msg: 'Internal Server Error'});
+      });
+  //
+  // const ref = await connectDB(admin, 'testDB');
+  // const db = admin.firestore();
+  // db.collection('BRAND').doc('products').set(customerProduct)
+  //     .then((response) => {
+  //       return res.status(200).json({msg: 'Added Successfully',
+  //         data: response});
+  //     })
+  //     .catch((err)=> {
+  //       console.error(err);
+  //       return res.status(500).json({msg: 'Internal Server Error'});
+  //     });
 });
 
 exports.FacebookCatalogTrigger = functions
-    .database// .instance('dev-adapter-353805')
-    .ref()
+    .firestore// .instance('dev-adapter-353805')
+    .document('BRAND/products')
     .onWrite((change, context) => {
-      const differences = detailedDiff(change.before.val(), change.after.val());
+      const differences = detailedDiff(
+          change.before.data(), change.after.data());
       const requests = [];
-      console.log(differences);
-      if (differences.added.products !== undefined) {
-        const addProducts = [];
-        addProducts.push(differences.added.products);
-        addProducts.forEach((add) => {
+      // console.log(differences);
+      if (Object.keys(differences.added).length !== 0) {
+        for (const [key, value] of Object.entries(differences.added)) {
           requests.push({
             method: 'CREATE',
             data: Object.assign(
                 {
-                  id: Object.keys(add)[0],
+                  id: key,
                 },
-                Object.values(add)[0]
+                value
             ),
           });
-        });
+        }
       }
-      if (differences.deleted.products !== undefined) {
-        const delProducts = [];
-        delProducts.push(differences.deleted.products);
-        delProducts.forEach((del) => {
+      if (Object.keys(differences.deleted).length !== 0) {
+        for (const [key, value] of Object.entries(differences.deleted)) {
           requests.push({
             method: 'DELETE',
-            data: Object.assign({
-              id: Object.keys(del)[0],
-            }),
+            data:
+                {
+                  id: key,
+                },
           });
-        });
+        }
       }
-      if (differences.updated.products !== undefined) {
-        const upProducts = [];
-        upProducts.push(differences.updated.products);
-        upProducts.forEach((up) => {
+      if (Object.keys(differences.updated).length !== 0) {
+        // const upProducts = [];
+        // upProducts.push(differences.updated);
+        for (const [key, value] of Object.entries(differences.updated)) {
           requests.push({
             method: 'UPDATE',
             data: Object.assign(
                 {
-                  id: Object.keys(up)[0],
+                  id: key,
                 },
-                Object.values(up)[0]
+                value
             ),
           });
-        });
+        }
       }
-      return console.log(requests);
-    //   // Cool Beans Long-lived Page Token
-    //   const accessToken =
-    //         'EAAECdwZBqHzEBAHaivpSFBMoQZBBWv1McHe9BcOluFNMthzf2zozFJmFAt57gNvwFaNHojLFvSK3nexhATlUifz1ZBxLXH90jPKYqwGZADtTohyFG589pUSevAEjwh0I8QFGrMjBfwWLAqQsthkKT8jWOH5gSh5F7c7sTkRNV8YmrRGuu1yPnMLVDIhr7yjJUQND3iOT1wZDZD';
-    //   const catalogId = '3256983894625954'; // Cool Beans Catalog ID
-    //   const apiVersion = 'v14.0';
-    //   const url = `https://graph.facebook.com/${apiVersion}/${catalogId}/items_batch`;
-    //   const payload = {
-    //     access_token: accessToken,
-    //     item_type: 'PRODUCT_ITEM',
-    //     requests,
-    //   };
-    //   return axios
-    //       .post(url, payload)
-    //       .then((response) => {
-    //         console.log(response);
-    //       })
-    //       .catch((err) => {
-    //         console.log(err);
-    //       });
+      // return console.log(requests);
+      // Cool Beans Long-lived Page Token
+      const accessToken =
+            'EAAECdwZBqHzEBAHaivpSFBMoQZBBWv1McHe9BcOluFNMthzf2zozFJmFAt57gNvwFaNHojLFvSK3nexhATlUifz1ZBxLXH90jPKYqwGZADtTohyFG589pUSevAEjwh0I8QFGrMjBfwWLAqQsthkKT8jWOH5gSh5F7c7sTkRNV8YmrRGuu1yPnMLVDIhr7yjJUQND3iOT1wZDZD';
+      const catalogId = '3256983894625954'; // Cool Beans Catalog ID
+      const apiVersion = 'v14.0';
+      const url = `https://graph.facebook.com/${apiVersion}/${catalogId}/items_batch`;
+      const payload = {
+        access_token: accessToken,
+        item_type: 'PRODUCT_ITEM',
+        requests,
+      };
+      return axios
+          .post(url, payload)
+          .then((response) => {
+            console.log(response);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
     });
 
 // exports.GoogleCatalogTrigger = functions.database
